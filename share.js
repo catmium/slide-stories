1c1d168732e11f80c10731b3855d8bbe86281b0e
Catmium <sirijets@gmail.com>
2022-10-02 23:59:22 +0700

    test img

--- share.js
+++ share.js
@@ -11,7 +11,7 @@ const output = document.getElementById('output')
 
 shareButton.addEventListener("click", async () => {
     const file = await urlToFile(
-      "https://images.unsplash.com/photo-1575535468632-345892291673?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
+      "./img/cat4.png"
     );
     const files = [file];
     console.log(files);
